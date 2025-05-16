e94959d368b44fe0c40022ddaa0e92b9699d76ca
zhengk30 <zhengk30@localhost>
2025-05-16 10:17:52 -0600

    now support cs on different machines

--- src/client.c
+++ src/client.c
@@ -1,10 +1,11 @@
 #include "socket.h"
 
 #define QUERY "SELECT COUNT(l_comment) FROM comment WHERE l_comment like '%regular%';"
+#define SERVER_IPADDR "128.110.216.209"
 
 int main() {
     printf("[client] setting up server's socket...\n");
-    int server_sock = client_init(PORT, IPADDR);
+    int server_sock = client_init(PORT, SERVER_IPADDR);
     printf("[client] server socket is %d, about to send a query to the server...\n", server_sock);
     client_send_query(server_sock, QUERY);
     uint64_t result = client_fetch_result(server_sock);
--- src/server.c
+++ src/server.c
@@ -1,12 +1,13 @@
 #include "socket.h"
 #include "third-party/duckdb/osx/duckdb.h"
 
-#define DBFILE_PATH "/Users/kaiwenzheng/Documents/csduck/inputs/tpch_lineitem_comment_sf1.db"
+#define CONNECTION_FROM "0.0.0.0"
+#define DBFILE_PATH "/users/zhengk30/csduck/inputs/tpch_lineitem_comment_sf1.db"
 
 void process_request(int client_sock);
 
 int main() {
-    int listen_fd = server_init(PORT, IPADDR);
+    int listen_fd = server_init(CONNECTION_FROM, IPADDR);
     printf("[server] listening on %d...\n", listen_fd);
     int client_sock = accept_connection(listen_fd);
     printf("[server] accepted connection from client %d\n", client_sock);
