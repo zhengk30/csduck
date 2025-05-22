67d6fdda3cc7d876ce2fdb088ad636e09b0bfd41
zhengk30 <zhengk30@localhost>
2025-05-22 06:55:38 -0600

    config script for setting up deps

--- config.sh
+++ config.sh
@@ -1,5 +1,20 @@
 #!/bin/bash
 
-LD_LIBRARY_PATH=/usr/local/lib
+so_path=/users/zhengk30/csduck/third-party/duckdb/
 
-export LD_LIBRARY_PATH=/users/zhengk30/csduck/third-party/duckdb/:$LD_LIBRARY_PATH
+## Unzip DuckDB's C/C++ API
+cd third-party/duckdb
+if ! [[ -f duckdb.h && -f duckdb.hpp && -f libduckdb.so ]]; then
+    unzip libduckdb-linux-aarch64.zip
+fi
+cd ../..
+
+## Set up LD_LIBRARY_PATH if needed
+if ! [[ $LD_LIBRARY_PATH == *"$so_path"* ]]; then
+    LD_LIBRARY_PATH=/usr/local/lib
+    export LD_LIBRARY_PATH=$so_path:$LD_LIBRARY_PATH
+fi
+# echo "$LD_LIBRARY_PATH"
+
+## TODO: Create `inputs` directory 
+
