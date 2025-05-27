542155774e7960f9b678ffb8bb72c1d559c65138
zhengk30 <zhengk30@localhost>
2025-05-26 23:45:31 -0600

    added script for compiling duckdb's source code

--- run.sh
+++ run.sh
@@ -1,17 +1,23 @@
 #!/bin/bash
 
+## Build DuckDB's source
+cd third-party/duckdb-source
+make
+cd ../..
+
+## Build client and server
 rm -rf build
 meson setup build
 meson compile -C build
 
-if [ "$1" = server ]; then
-    if [ "$#" -ne 2 ]; then
-        build/server
-    else
-        build/server "$2"
-    fi
-elif [ "$1" = client ]; then
-    build/client
-else
-    echo "usage: ./run.sh server | client"
-fi
+# if [ "$1" = server ]; then
+#     if [ "$#" -ne 2 ]; then
+#         build/server
+#     else
+#         build/server "$2"
+#     fi
+# elif [ "$1" = client ]; then
+#     build/client
+# else
+#     echo "usage: ./run.sh server | client"
+# fi
