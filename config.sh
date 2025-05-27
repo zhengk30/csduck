22502f0cd21ef1872f8d92690d9f0b43ead1bdf5
Kaiwen Zheng <115329498+zhengk30@users.noreply.github.com>
2025-05-27 10:06:12 +0800

    added db file generator

--- config.sh
+++ config.sh
@@ -2,8 +2,12 @@
 
 common_path=/users/zhengk30/csduck/
 duckdb_path=/users/zhengk30/.duckdb/
+duckdb_cli_path=/users/zhengk30/.duckdb/cli/latest
+
 so_path=${common_path}third-party/duckdb/
 inputs_path=${common_path}inputs/
+db_file_path=${common_path}inputs/comment_sf1.db
+duckdb_api_path=${comment_path}third-party/duckdb
 
 ## Unzip DuckDB's C/C++ API
 cd third-party/duckdb
@@ -22,10 +26,22 @@ fi
 ## Install DuckDB's CLI
 if ! [ -d "$duckdb_path" ]; then
     curl https://install.duckdb.org | sh
-    export PATH='/users/zhengk30/.duckdb/cli/latest':$PATH
+fi
+
+if ! [[ $PATH == *"$duckdb_cli_path"* ]]; then
+    export PATH=$duckdb_cli_path:$PATH
 fi
 
 ## Create `inputs` directory if none exists
 if ! [ -d "$inputs_path" ]; then
     mkdir $inputs_path
 fi
+
+## Create a database file if none exists
+if ! [ -e "$db_file_path" ]; then
+    gcc dbgen.c -o dbgen -L${duckdb_api_path} -lduckdb
+    ./dbgen
+    rm -f comment_sf1.csv
+    rm -f dbgen
+fi
+
