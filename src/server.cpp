e2ebb2a94a22884b01fa9b12b84554199bb4c8c8
Kaiwen Zheng <115329498+zhengk30@users.noreply.github.com>
2025-06-02 14:49:53 +0800

    added support for collecting stats for in-memory db

--- src/server.cpp
+++ src/server.cpp
@@ -6,7 +6,9 @@
 using json = nlohmann::json;
 
 #define CONNECTION_FROM "0.0.0.0"
-#define DBFILE_PATH "/users/zhengk30/csduck/inputs/comment_sf5.db"
+// #define DBFILE_PATH "/users/zhengk30/csduck/inputs/sf10.db"
+#define DBFILE_PATH "/proj/fardatalab-PG0/dbfiles/sf100.db"
+#define IMDB_PATH "/proj/fardatalab-PG0/exported-data/sf1"
 
 void process_request(int client_sock, bool prof_brkdown_enabled);
 
@@ -25,6 +27,76 @@ int main(int argc, char** argv) {
     process_request(client_sock, prof_brkdown_enabled);
     tear_down_connection(listen_fd);
     return 0;
+}
+
+void process_request(int client_sock, const char* src, bool prof_brkdown_enabled) {
+    char query[BUF_SIZE];
+    int bytes_read = read(client_sock, query, BUF_SIZE);
+    if (bytes_read < 0) {
+        perror("read");
+        tear_down_connection(client_sock);
+        exit(1);
+    }
+    std::cout << "[server] received " << bytes_read << " bytes from client with fd " << client_sock << '\n';;
+
+    char import_query[1024] = {0};
+    char* import = "IMPORT DATABASE '";
+    memcpy(import_query, import, strlen(import));
+    memcpy(import_query + strlen(import), src, strlen(src));
+    memcpy(import_query + strlen(import) + strlen(src), "';", 2);
+    printf("import query: %s\n", import_query);
+
+    // connect to duckdb and run query
+    duckdb_database db;
+    duckdb_connection conn;
+    duckdb_result result;
+    
+    assert(duckdb_open(NULL, &db) != DuckDBError);
+    assert(duckdb_connect(db, &conn) != DuckDBError);
+    assert(duckdb_query(conn, import_query, NULL) != DuckDBError);
+
+    if (prof_brkdown_enabled) {
+        duckdb_query(conn, "PRAGMA enable_profiling='json';", NULL);
+        duckdb_query(conn, "PRAGMA profiling_output='output.json';", NULL);
+    }
+    // execute query
+    auto start = chrono::high_resolution_clock::now();
+    assert(duckdb_query(conn, query, &result) != DuckDBError);
+    auto end = chrono::high_resolution_clock::now();
+    std::chrono::duration<double> elapsed = end - start;
+    std::cout << "[server] query execution duration: " << elapsed.count() << " sec\n";
+
+    // retrive result from row 0, column 0 of the returned table
+    uint64_t count = 0;
+    if (duckdb_row_count(&result) > 0) {
+        count = duckdb_value_uint64(&result, 0, 0);
+    }
+    
+    // reply to client with count
+    int bytes_written = write(client_sock, &count, sizeof(uint64_t));
+    if (bytes_written < 0) {
+        perror("write");
+        tear_down_connection(client_sock);
+        exit(1);
+    }
+    duckdb_destroy_result(&result);
+    std::cout << "[server] replied with " << bytes_written << " bytes\n";
+
+    if (prof_brkdown_enabled) {
+        std::ifstream output("output.json");
+        json data = json::parse(output);
+        auto latency = data["latency"];
+        auto cpu_time = data["cpu_time"];
+        auto aggregate_cpu_time = data["children"][0]["operator_timing"];
+        auto filter_cpu_time = data["children"][0]["children"][0]["operator_timing"];
+        auto scan_cpu_time = data["children"][0]["children"][0]["children"][0]["operator_timing"];
+        std::cout << "======= Timing breakdown =======\n";
+        std::cout << "[server] execution latency (e2e): " << latency << " sec\n";
+        std::cout << "[server] execution latency (cpu): " << cpu_time << " sec\n";
+        std::cout << "[server] aggregate operator latency: " << aggregate_cpu_time << " sec\n";
+        std::cout << "[server] filter operator latency: " << filter_cpu_time << " sec\n";
+        std::cout << "[server] scan operator latency: " << scan_cpu_time << " sec\n";
+    }
 }
 
 void process_request(int client_sock, bool prof_brkdown_enabled) {
@@ -69,7 +141,7 @@ void process_request(int client_sock, bool prof_brkdown_enabled) {
     std::chrono::duration<double> elapsed = end - start;
     std::cout << "[server] query execution duration: " << elapsed.count() << " sec\n";
 
-    // retrive result from row 0, column 0 of the returned table
+    // retrieve result from row 0, column 0 of the returned table
     uint64_t count = 0;
     if (duckdb_row_count(&result) > 0) {
         count = duckdb_value_uint64(&result, 0, 0);
@@ -93,6 +165,7 @@ void process_request(int client_sock, bool prof_brkdown_enabled) {
         auto aggregate_cpu_time = data["children"][0]["operator_timing"];
         auto filter_cpu_time = data["children"][0]["children"][0]["operator_timing"];
         auto scan_cpu_time = data["children"][0]["children"][0]["children"][0]["operator_timing"];
+        std::cout << "======= Timing breakdown =======\n";
         std::cout << "[server] execution latency (e2e): " << latency << " sec\n";
         std::cout << "[server] execution latency (cpu): " << cpu_time << " sec\n";
         std::cout << "[server] aggregate operator latency: " << aggregate_cpu_time << " sec\n";
